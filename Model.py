e4e06536a8fb367f681b4a9f53e95c8012a1cd0a
archanjojr <math.calazans56@gmail.com>
2017-10-12 09:24:19 -0300

    todos os arquivos iniciais

---
+++ Model.py
@@ -0,0 +1,16 @@
+import math as mat
+
+def distribuicao_nominal(n,k,p,q):
+    """
+    N= numero total de provas
+    K= numero de vezes que se quer a ocorrencia
+    P= probabilidade de sucesso da ocorrencia
+    Q= probabilidade de fracasso da ocorrencia
+    PERCENT= SE DESEJA O RESULTADO EM PORCENTAGEM(default=FALSE)
+    """
+    a=mat.factorial(n)/(mat.factorial(k)*mat.factorial(n-k))
+    b=(p**k)*(q**(n-k))
+    result=a*b
+    print('P(X='+str(k)+')='+str(result))
+
+distribuicao_nominal(6,6,41/100,59/100)
