93f1af9bd02e6214f4a58c0bafddda4f60058bf5
archanjojr <math.calazans56@gmail.com>
2017-10-17 19:36:40 -0200

    add readme.md, adicionado return na distribuição nominal

--- Main.py
+++ Main.py
@@ -1,9 +1,16 @@
 import Model
-from Model import probabilidade as pb
-
-'''lista='10 11 13 13 14 16 16 17 17 19 20 20 21 21 22 23 23 23 24 25 25 26 27 27 28 29 30 30 31 32 32 32 33 33 34 ' \
+import tkinter
+from tkinter.constants import *
+lista='10 11 13 13 14 16 16 17 17 19 20 20 21 21 22 23 23 23 24 25 25 26 27 27 28 29 30 30 31 32 32 32 33 33 34 ' \
       '35 36 38 38 39 40 41 43 44 45 45 46 48 49 50 52 53 53 54 58 62 63 65 70 78'
 r = Model.rol(lista)
-print(Model.amplitude_cla(r))'''
+aux = len(r)
+jan1=tkinter.Tk()
+jan1.title("Distribuição Nominal")
+jan1lbn = tkinter.Label(jan1,text='N',font=('comic sans',40))
+jan1lbk = tkinter.Label(jan1,text='K')
+jan1lbp = tkinter.Label(jan1,text='P')
+jan1lbq = tkinter.Label(jan1,text='Q')
 
-print(Model.probabilidadeC(pb(9,20),pb(1,5)))
+jan1.mainloop()
+
--- Model.py
+++ Model.py
@@ -6,12 +6,13 @@ def distribuicao_nominal(n,k,p,q):
     K= numero de vezes que se quer a ocorrencia
     P= probabilidade de sucesso da ocorrencia
     Q= probabilidade de fracasso da ocorrencia
+    :return float(or int)
     """
     a = mat.factorial(n)/(mat.factorial(k)*mat.factorial(n-k))
     b = (p**k) * (q**(n-k))
     result = a*b
     print('P(X='+str(k)+')='+str(result))
-
+    return result
 
 def rol(s):
     '''
